e55c5bd465f36547447e00e3f817ca0a170ff3fc
Folu Obatusin <nisutabo@gmail.com>
2018-01-04 03:42:31 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |category, types|
+    types.each do |type, names|
+      names.each do |name|
+        pigeon_list[name] = {} if pigeon_list[name] == nil
+        pigeon_list[name][category] = [] if pigeon_list[name][category] == nil
+        pigeon_list[name][category] << type.to_s
+      end
+    end
+  end
+  pigeon_list
 end
